6e956b6af30351492b91ff879b7dd9e5fe05a1c5
N1K@ <nikanatroshvili7@gmail.com>
2022-11-29 22:56:44 +0400

    add @Getter and @Setter annotations (Lombok)

--- src/main/java/first/springboot/springboot/entity/Customer.java
+++ src/main/java/first/springboot/springboot/entity/Customer.java
@@ -1,52 +1,18 @@
 package first.springboot.springboot.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
+
+@Getter
+@Setter
 
 public class Customer {
     private Integer id;
     private  String firstName;
     private  String lastName;
     private LocalDate birthdate;
-
     private Boolean deleted;
 
-    public Boolean getDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(Boolean deleted) {
-        this.deleted = deleted;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
-    }
 }
