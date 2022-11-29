65d33efeb523a85ea540fd18f15c73183e44f2f1
N1K@ <nikanatroshvili7@gmail.com>
2022-11-29 21:44:34 +0400

    first springboot

---
+++ src/main/java/first/springboot/springboot/controller/CustomerController.java
@@ -0,0 +1,52 @@
+package first.springboot.springboot.controller;
+
+import first.springboot.springboot.entity.Customer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+    private static int id = 0;
+    private List<Customer> db = new ArrayList<>();
+
+    @GetMapping ("/customers")
+    public List<Customer> getAll() {
+        return db.stream().filter(c -> !c.getDeleted()).toList();
+    }
+
+    @PostMapping ("/customers")
+    public Customer add(@RequestBody  Customer customer) {
+        customer.setId(++id);
+        customer.setDeleted(false);
+        db.add(customer);
+        return customer;
+    }
+
+    @PutMapping ("/customers/{id}")
+    public ResponseEntity<Customer> update(@RequestBody Customer customer, @PathVariable int id) {
+        var optional = db.stream().filter(c -> c.getId() == id).findFirst();
+        if(optional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        var foundCustomer = optional.get();
+        foundCustomer.setFirstName(customer.getFirstName());
+        foundCustomer.setLastName(customer.getLastName());
+        foundCustomer.setBirthdate(customer.getBirthdate());
+        return ResponseEntity.ok(foundCustomer);
+    }
+
+    @DeleteMapping ("/customers/{id}")
+    public ResponseEntity<Customer> delete(@PathVariable int id) {
+        var optional = db.stream().filter(c -> c.getId() == id).findFirst();
+        if(optional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        var foundCustomer = optional.get();
+        foundCustomer.setDeleted(true);
+        return ResponseEntity.noContent().build();
+
+    }
+}
---
+++ src/main/java/first/springboot/springboot/entity/Customer.java
@@ -0,0 +1,52 @@
+package first.springboot.springboot.entity;
+
+import java.time.LocalDate;
+
+public class Customer {
+    private Integer id;
+    private  String firstName;
+    private  String lastName;
+    private LocalDate birthdate;
+
+    private Boolean deleted;
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+}
